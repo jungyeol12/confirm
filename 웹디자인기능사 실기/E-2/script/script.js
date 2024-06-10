a275aaf5d2b5413a247af93568b79ab86a29c5a8
jungyeol12 <jbae23233@gmail.com>
2024-06-10 22:00:55 +0900

    E-2
    E-2 유형

---
+++ 웹디자인기능사 실기/E-2/script/script.js
@@ -0,0 +1,37 @@
+$(".menu ul li").hover(
+    function() {
+        $(this).children("ul").stop().slideDown("slow");
+    },
+    function() {
+        $(this).children("ul").stop().slideUp("fast");
+    }
+)
+
+var slide = $(".slide > div");
+var index = 0;
+var size = slide.length - 1;
+var timer = setInterval("autoslide()", 1500);
+
+function autoslide() {
+    $(slide[index]).stop().animate({
+        "top": "-100%"
+    }, 1000, function() {
+        $(this).css({top: "100%"})
+    });
+    index++;
+    if (index > size) index = 0;
+    $(slide[index]).stop().animate({
+        "top": "0"
+    }, 1000);
+}
+
+$(".openPop").click(
+    function() {
+        $("#popup").show();
+    }
+)
+$(".closePop").click(
+    function() {
+        $("#popup").hide();
+    }
+)
