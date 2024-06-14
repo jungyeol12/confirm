2289f67ea04e3638ecca1bf58d1ac225c576e1cd
jungyeol12 <jbae23233@gmail.com>
2024-06-14 23:24:40 +0900

    A-1

--- 웹디자인기능사 실기/A-1/js/script.js
+++ 웹디자인기능사 실기/A-1/js/script.js
@@ -5,53 +5,49 @@ $(".menu").hover(
     function() {
         $(".sub-menu").stop().slideUp("fast");
     }
-);
+)
 
-var slide = $(".slider");
+var slide = $(".slide img");
 var index = 0;
 var size = slide.length - 1;
-var timer = setInterval("autoslide()", 3000);
+var timer = setInterval("autoslide()", 2000);
 
 function autoslide() {
     $(slide[index]).stop().animate({
-        top: "300px"
+        top: "-100%"
     }, 1000, function() {
-        $(this).css({top: "-300px"});
+        $(this).css({top: "100%"});
     });
     index++;
-    if(index > size) {
-        index = 0;
-    }
+    if(index > size) index = 0;
     $(slide[index]).stop().animate({
-        top: 0
+        top: "0"
     }, 1000);
 }
 
-$(".tab-news").click(function() {
-    $(this).css({
-        "border-bottom":"0",
-        "background-color":"white"
-    });
-    $(".tab-gallery").css({
-        "border-bottom":"1px solid #000",
-        "background-color": "#888"
-    });
-    $(".gallery").hide();
-    $(".news").show();
-})
+$(".popup-open").on("click", function() {
+    $("#popup").css("display", "flex");
+});
+$(".popup-close").on("click", function() {
+    $("#popup").css("display", "none");
+});
 
-$(".tab-gallery").click(function() {
-    $(this).css({
-        "border-bottom":"0",
-        "background-color":"white"
-    });
-    $(".tab-news").css({
-        "border-bottom":"1px solid #000",
-        "background-color": "#888"
-    });
-    $(".gallery").css({
-        "display":"flex"
-    });
-    $(".news").hide();
-    $(".gallery").show();
-})
+$(".tab ul li:first-child").click(
+    function() {
+        $(this).addClass("on");
+        $(".tab ul li:last-child").removeClass("on");
+
+        $(".notice").show();
+        $(".gallery").hide();
+    }
+)
+
+$(".tab ul li:last-child").click(
+    function() {
+        $(this).addClass("on");
+        $(".tab ul li:first-child").removeClass("on");
+
+        $(".notice").hide();
+        $(".gallery").show();
+    }
+)
